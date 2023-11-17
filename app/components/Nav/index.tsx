445cf8de009883cb5ca74b5948f0a579d6cfaa1c
Blake Tarter <blakertarter@gmail.com>
2023-11-17 15:16:16 -0600

    Add basic dark/light mode theme switch

--- app/components/Nav/index.tsx
+++ app/components/Nav/index.tsx
@@ -1,5 +1,6 @@
 import FadeIn from "../FadeIn"
 import ScrollLink from "../ScrollLink"
+import ThemeSwitcher from "../ThemeSwitcher"
 import styles from "./index.module.css"
 
 const navItems = [
@@ -25,15 +26,20 @@ export default function Nav() {
   return (
     <nav className={styles.navWrapper}>
       <ul className={styles.navInner}>
-        {navItems.map((item) => (
-          <li className={styles.navItem} key={item.text}>
-            <FadeIn delay={1500} direction="down">
-              <ScrollLink className={styles.navLink} href={item.href}>
-                {item.text}
-              </ScrollLink>
-            </FadeIn>
-          </li>
-        ))}
+        <FadeIn delay={1500} direction="down">
+          <ThemeSwitcher />
+        </FadeIn>
+        <div className={styles.navItems}>
+          {navItems.map((item) => (
+            <li className={styles.navItem} key={item.text}>
+              <FadeIn delay={1700} direction="down">
+                <ScrollLink className={styles.navLink} href={item.href}>
+                  {item.text}
+                </ScrollLink>
+              </FadeIn>
+            </li>
+          ))}
+        </div>
       </ul>
     </nav>
   )
